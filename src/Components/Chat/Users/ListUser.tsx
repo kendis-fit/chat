9f416b58e434183606855d8536b3712c71a292c1
Kurt Endless <kendis_it@hotmail.com>
2020-01-22 12:47:44 +0200

    update: component list user

--- src/Components/Chat/Users/ListUser.tsx
+++ src/Components/Chat/Users/ListUser.tsx
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import ItemUser from "./ItemUser";
+import IUser from "../Interfaces/IUser";
 import IListUser from "../Interfaces/IListUser";
 
 const ListUser = (props: IListUser) => {
 
     const [users, setUsers] = useState(props.Users);
+
+    useEffect(() => {
+
+        props.Socket.on("receiveUser", (user: IUser) => setUsers([...users, user]));
+
+    }, [users, props]);
 
     return(
         <ul>
